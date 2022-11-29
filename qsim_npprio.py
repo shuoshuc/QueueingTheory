83bb4c576caa89885b845b5254fe867e707004b3
Shawn Chen <sschenshawn@gmail.com>
2022-11-28 19:09:34 -0500

    Final report simulations

--- qsim_npprio.py
+++ qsim_npprio.py
@@ -36,44 +36,26 @@ class Job:
     jid: int
     t_arrival: float
     service_time: float
+    job_type: int
 
     def __post_init__(self):
         '''
         Priority queue across buckets, FCFS within the same bucket.
         '''
-        self.sort_index = self.bucket(self.service_time)
-
-    def bucket(self, service_time):
-        if service_time > 20000:
-            return 3
-        elif service_time > 4000:
-            return 2
-        else:
-            return 1
+        self.sort_index = self.job_type
 
 class JobQueue:
     '''
-    Unbounded PS job queue implementation.
+    Unbounded NP-Prio job queue implementation.
     '''
-    def __init__(self, current_time):
+    def __init__(self):
         self.q = []
-        self.q_clock = current_time
 
     def enqueue(self, job):
-        if len(self.q):
-            top_job = heapq.heappop(self.q)
-            # Update first job's service time.
-            top_job.service_time -= job.t_arrival - self.q_clock
-            heapq.heappush(self.q, top_job)
-        # New job's arrival time is the current clock.
-        self.q_clock = job.t_arrival
         heapq.heappush(self.q, job)
 
-    def dequeue(self, current_time):
-        completed_job = heapq.heappop(self.q)
-        # Updates clock as job leaves queue.
-        self.q_clock = current_time
-        return completed_job
+    def dequeue(self):
+        return heapq.heappop(self.q)
 
     def peek(self):
         if len(self.q):
@@ -86,23 +68,29 @@ class JobQueue:
         return len(self.q)
 
 class QSim:
-    def __init__(self):
+    def __init__(self, rho):
+        self.rho = rho
         self._rng = default_rng()
         self.num_jobs = 0
         self.clock = 0.0
         self.t_next_arrival = self.clock + self._genIAT()
         self.t_next_completion = float('inf') 
+        self.job_type = -1
 
-        # A SRPT queue tracking the arrival times of each job, for computing the
+        # A NP-Prio queue tracking the arrival times of each job, for computing the
         # *response* time later.
-        self.q = JobQueue(self.clock)
-        self.total_response_time = 0
+        self.q = JobQueue()
+        self.total_response_time_h = 0
+        self.total_response_time_l = 0
         # Number of jobs fully completed.
-        self.served_jobs = 0
+        self.served_jobs_h = 0
+        self.served_jobs_l = 0
 
         self.global_jid = AtomicCounter()
         # Job id of the current job under service.
         self.curr_jid = -1
+        # Arrival time of the current job under service.
+        self.t_curr_arrival = float('inf')
 
         # Registers event handlers.
         self._event_handlers = {
@@ -116,28 +104,20 @@ class QSim:
         time.
         Note: assuming M/G/1 queue, IAT ~ Exp(1/scale).
         '''
-        '''
-        return self._rng.exponential(scale=1/0.8)
-        '''
-        rho = 0.6
-        return self._rng.exponential(scale=(2999.99/rho))
+        return self._rng.exponential(scale=(16.2/self.rho))
 
     def _genServiceTime(self):
         '''
         Generates the service time for a job.
         Note: assuming M/G/1 queue, service time ~ DegenerateHyperExp(mu=1, p).
         '''
-        '''
-        p = 1/4
-        # With probability 1 - p, service time is 0.
-        if self._rng.uniform(low=0, high=1) > p:
-            return 0
-        return self._rng.exponential(scale=1/p)
-        '''
-        # Bounded Pareto distribution
-        a, k, p = 1.8, 1333.33, 1e11
-        s = (self._rng.pareto(a) + 1) * k
-        return s / (1 - np.power(k/p, a)) if s > 7.58861e-23 else 0
+        # With prob 0.2, return topo size, else demand size.
+        if self._rng.uniform(low=0, high=1) < 0.2:
+            a, m = 2, 0.5
+            s = (self._rng.pareto(a) + 1) * m
+            return (s, 1)
+        else:
+            return (self._rng.exponential(scale=(1/0.05)), 1000000)
 
     def _eventHandler(self, event_type):
         '''
@@ -150,11 +130,15 @@ class QSim:
         Updates states in response to an arrival event.
         '''
         self.num_jobs += 1
-        self.q.enqueue(Job(self.global_jid.increment(), self.clock, self._genServiceTime()))
-        # Find out which job has the shortest remaining processing time.
-        job = self.q.peek()
-        self.curr_jid = job.jid
-        self.t_next_completion = self.clock + job.service_time
+        service_time, job_type = self._genServiceTime()
+        self.q.enqueue(Job(self.global_jid.increment(), self.clock,
+                           service_time, job_type))
+        if self.num_jobs == 1:
+            job = self.q.dequeue()
+            self.curr_jid = job.jid
+            self.t_curr_arrival = job.t_arrival
+            self.t_next_completion = self.clock + job.service_time
+            self.job_type = job.job_type
         self.t_next_arrival = self.clock + self._genIAT()
         
     def _completionHelper(self):
@@ -162,17 +146,24 @@ class QSim:
         Updates states in response to a completion event.
         '''
         self.num_jobs -= 1
-        completed_job = self.q.dequeue(self.clock)
-        self.total_response_time += self.clock - completed_job.t_arrival
-        self.served_jobs += 1
+        if self.job_type > 1:
+            self.total_response_time_l += self.clock - self.t_curr_arrival
+            self.served_jobs_l += 1
+        else:
+            self.total_response_time_h += self.clock - self.t_curr_arrival
+            self.served_jobs_h += 1
         if self.num_jobs > 0:
             # Continue serving the rest jobs.
-            next_job = self.q.peek()
+            next_job = self.q.dequeue()
             self.curr_jid = next_job.jid
+            self.t_curr_arrival = next_job.t_arrival
             self.t_next_completion = self.clock + next_job.service_time
+            self.job_type = next_job.job_type
         else:
             self.curr_jid = -1
-            self.t_next_completion = float("inf")
+            self.t_curr_arrival = float('inf')
+            self.t_next_completion = float('inf')
+            self.job_type = -1
     
     def run(self):
         '''
@@ -189,18 +180,28 @@ class QSim:
 
 
 if __name__ == "__main__":
-    qsim = QSim()
+    for rho in [float(0.01 * i) for i in range(1, 100)]:
+        qsim = QSim(rho)
 
-    print(f"[Start] clock={qsim.clock}, # jobs: {qsim.num_jobs}, next arrival: " 
-          f"{qsim.t_next_arrival}, next completion: {qsim.t_next_completion}")
+        '''
+        print(f"[Start] clock={qsim.clock}, # jobs: {qsim.num_jobs}, next arrival: " 
+              f"{qsim.t_next_arrival}, next completion: {qsim.t_next_completion}")
+        '''
 
-    for i in range(NUM_ITER):
-        qsim.run()
-        if qsim.served_jobs:
-            ET = qsim.total_response_time / qsim.served_jobs
-        else:
-            ET = None
-        print(f"[Iter {i}] clock={qsim.clock}, # jobs: {qsim.num_jobs}, "
-              f"next arrival: {qsim.t_next_arrival}, next completion: "
-              f"{qsim.t_next_completion}, current job: {qsim.curr_jid}, "
-              f"E[T]={ET}")
+        for i in range(NUM_ITER):
+            qsim.run()
+            if qsim.served_jobs_h:
+                ETH = qsim.total_response_time_h / qsim.served_jobs_h
+            else:
+                ETH = None
+            if qsim.served_jobs_l:
+                ETL = qsim.total_response_time_l / qsim.served_jobs_l
+            else:
+                ETL = None
+            '''
+            print(f"[Iter {i}] clock={qsim.clock}, # jobs: {qsim.num_jobs}, "
+                  f"next arrival: {qsim.t_next_arrival}, next completion: "
+                  f"{qsim.t_next_completion}, current job: {qsim.curr_jid}, "
+                  f"E[T]={ET}")
+            '''
+        print(f"{rho}, {ETH}, {ETL}")
--- qsim_pprio_finite.py
+++ qsim_pprio_finite.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import copy
 import heapq
 import queue
+import sys
 import threading
 from dataclasses import dataclass, field
 from enum import Enum
@@ -11,6 +13,8 @@ import numpy as np
 from numpy.random import default_rng
 
 NUM_ITER = 1000000
+
+BUF_SIZE = 10
 
 class EventType(Enum):
     ARRIVAL = 1
@@ -36,41 +40,55 @@ class Job:
     jid: int
     t_arrival: float
     service_time: float
+    job_type: int
 
     def __post_init__(self):
         '''
         Priority queue across buckets, FCFS within the same bucket.
         '''
-        self.sort_index = self.bucket(self.service_time)
-
-    def bucket(self, service_time):
-        if service_time > 20000:
-            return 3
-        elif service_time > 4000:
-            return 2
-        else:
-            return 1
+        self.sort_index = self.job_type
 
 class JobQueue:
     '''
-    Unbounded PS job queue implementation.
+    Unbounded P-Prio job queue implementation.
     '''
     def __init__(self, current_time):
         self.q = []
         self.q_clock = current_time
+        # Number of low priority jobs in system (queue + server).
+        self.cnt_l = 0
 
     def enqueue(self, job):
+        hasDrop = False
         if len(self.q):
             top_job = heapq.heappop(self.q)
             # Update first job's service time.
             top_job.service_time -= job.t_arrival - self.q_clock
+            # Make sure preempted job is ranked first among its peer.
+            top_job.job_type -= 1
             heapq.heappush(self.q, top_job)
+        # Low priority queue is full, need to kick one job out.
+        if self.cnt_l >= BUF_SIZE and job.job_type > 1:
+            #victim = sorted([j for j in self.q if j.job_type >= 1000000],
+            victim = sorted([j for j in self.q if j.job_type >= 1],
+                            key=lambda x: x.t_arrival)[0]
+            self.q.remove(victim)
+            self.q.append(job)
+            # Re-heapify
+            heapq.heapify(self.q)
+            hasDrop = True
+        else:
+            if job.job_type > 1:
+                self.cnt_l += 1
+            heapq.heappush(self.q, job)
         # New job's arrival time is the current clock.
         self.q_clock = job.t_arrival
-        heapq.heappush(self.q, job)
+        return hasDrop
 
     def dequeue(self, current_time):
         completed_job = heapq.heappop(self.q)
+        if completed_job.job_type > 1:
+            self.cnt_l -= 1
         # Updates clock as job leaves queue.
         self.q_clock = current_time
         return completed_job
@@ -86,19 +104,24 @@ class JobQueue:
         return len(self.q)
 
 class QSim:
-    def __init__(self):
+    def __init__(self, rho):
+        self.rho = rho
         self._rng = default_rng()
         self.num_jobs = 0
         self.clock = 0.0
         self.t_next_arrival = self.clock + self._genIAT()
         self.t_next_completion = float('inf') 
 
-        # A SRPT queue tracking the arrival times of each job, for computing the
+        # A P-Prio queue tracking the arrival times of each job, for computing the
         # *response* time later.
         self.q = JobQueue(self.clock)
-        self.total_response_time = 0
+        self.total_response_time_h = 0
+        self.total_response_time_l = 0
         # Number of jobs fully completed.
-        self.served_jobs = 0
+        self.served_jobs_h = 0
+        self.served_jobs_l = 0
+        self.inter_completion_time = 0
+        self.t_last_complete_l = -1
 
         self.global_jid = AtomicCounter()
         # Job id of the current job under service.
@@ -116,28 +139,20 @@ class QSim:
         time.
         Note: assuming M/G/1 queue, IAT ~ Exp(1/scale).
         '''
-        '''
-        return self._rng.exponential(scale=1/0.8)
-        '''
-        rho = 0.6
-        return self._rng.exponential(scale=(2999.99/rho))
+        return self._rng.exponential(scale=(16.2/self.rho))
 
     def _genServiceTime(self):
         '''
         Generates the service time for a job.
         Note: assuming M/G/1 queue, service time ~ DegenerateHyperExp(mu=1, p).
         '''
-        '''
-        p = 1/4
-        # With probability 1 - p, service time is 0.
-        if self._rng.uniform(low=0, high=1) > p:
-            return 0
-        return self._rng.exponential(scale=1/p)
-        '''
-        # Bounded Pareto distribution
-        a, k, p = 1.8, 1333.33, 1e11
-        s = (self._rng.pareto(a) + 1) * k
-        return s / (1 - np.power(k/p, a)) if s > 7.58861e-23 else 0
+        # With prob 0.2, return topo size, else demand size.
+        if self._rng.uniform(low=0, high=1) < 0.2:
+            a, m = 2, 0.5
+            s = (self._rng.pareto(a) + 1) * m
+            return (s, 1)
+        else:
+            return (self._rng.exponential(scale=(1/0.05)), 1000000)
 
     def _eventHandler(self, event_type):
         '''
@@ -150,8 +165,12 @@ class QSim:
         Updates states in response to an arrival event.
         '''
         self.num_jobs += 1
-        self.q.enqueue(Job(self.global_jid.increment(), self.clock, self._genServiceTime()))
-        # Find out which job has the shortest remaining processing time.
+        service_time, job_type = self._genServiceTime()
+        hasDrop = self.q.enqueue(Job(self.global_jid.increment(), self.clock,
+                                     service_time, job_type))
+        # One job is kicked out so account for that.
+        if hasDrop:
+            self.num_jobs -= 1
         job = self.q.peek()
         self.curr_jid = job.jid
         self.t_next_completion = self.clock + job.service_time
@@ -163,8 +182,15 @@ class QSim:
         '''
         self.num_jobs -= 1
         completed_job = self.q.dequeue(self.clock)
-        self.total_response_time += self.clock - completed_job.t_arrival
-        self.served_jobs += 1
+        if completed_job.job_type > 1:
+            self.total_response_time_l += self.clock - completed_job.t_arrival
+            if self.t_last_complete_l != -1:
+                self.inter_completion_time += self.clock - self.t_last_complete_l
+            self.t_last_complete_l = self.clock
+            self.served_jobs_l += 1
+        else:
+            self.total_response_time_h += self.clock - completed_job.t_arrival
+            self.served_jobs_h += 1
         if self.num_jobs > 0:
             # Continue serving the rest jobs.
             next_job = self.q.peek()
@@ -189,18 +215,31 @@ class QSim:
 
 
 if __name__ == "__main__":
-    qsim = QSim()
+    BUF_SIZE = int(sys.argv[1])
+    for rho in [float(0.01 * i) for i in range(1, 100)]:
+        qsim = QSim(rho)
 
-    print(f"[Start] clock={qsim.clock}, # jobs: {qsim.num_jobs}, next arrival: " 
-          f"{qsim.t_next_arrival}, next completion: {qsim.t_next_completion}")
+        '''
+        print(f"[Start] clock={qsim.clock}, # jobs: {qsim.num_jobs}, next arrival: " 
+              f"{qsim.t_next_arrival}, next completion: {qsim.t_next_completion}")
+        '''
 
-    for i in range(NUM_ITER):
-        qsim.run()
-        if qsim.served_jobs:
-            ET = qsim.total_response_time / qsim.served_jobs
-        else:
-            ET = None
-        print(f"[Iter {i}] clock={qsim.clock}, # jobs: {qsim.num_jobs}, "
-              f"next arrival: {qsim.t_next_arrival}, next completion: "
-              f"{qsim.t_next_completion}, current job: {qsim.curr_jid}, "
-              f"E[T]={ET}")
+        for i in range(NUM_ITER):
+            qsim.run()
+            if qsim.served_jobs_h:
+                ETH = qsim.total_response_time_h / qsim.served_jobs_h
+            else:
+                ETH = None
+            if qsim.served_jobs_l:
+                ETL = qsim.total_response_time_l / qsim.served_jobs_l
+                EIC = qsim.inter_completion_time / qsim.served_jobs_l
+            else:
+                ETL = None
+                EIC = None
+            '''
+            print(f"[Iter {i}] clock={qsim.clock}, # jobs: {qsim.num_jobs}, "
+                  f"next arrival: {qsim.t_next_arrival}, next completion: "
+                  f"{qsim.t_next_completion}, current job: {qsim.curr_jid}, "
+                  f"E[TH]={ETH}")
+            '''
+        print(f"{rho}, {ETH}, {ETL}, {EIC}")
